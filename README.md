d28a08b983276b3c57aa1d7ec8d718fc496da006
Ricardo Ortega <Ricardo.Ortega.-ND@disney.com>
2022-08-17 17:51:08 -0500

    Adding README instructions and test logs.

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# generic-pageable
+# Generic Pagination
+
+### Pagination functionality provided by Spring Data JPA without querying databases. 
+
+The **Pageable** object included in Spring Boot JPA dependency allow us to implement the pagination functionality converting this object in to SQL statements.
+
+This Utils class allow us to apply the Pageable attributes to a list of objects. 
+
+How to use it...
+
+Create a pageUtil for the type of objects that you want to fetch in Pages 
+
+```java
+PageUtil<MyObjectDto> pageUtil = new PageUtil<>();
+```
+
+The **listToPage** method receives a list of objects and returns the requested Page of object.
+
+```java
+//Pageable object with options defined by the client (page, size and sort)
+Pageable pageable...
+        
+//List of objects retrieved from an external data source
+List<MyObjectDto> objectsList = externalApiService.getAllUsersData();
+
+//Applying the pageable options to the object lists
+Page<MyObjectDto> objectsPage = listToPage(objectsList, pageable);
+```
+
+**NOTE: Check the included unit tests to know more about the uses cases covered by this Utils class.**
+
+Run PageUtils tests using Apache Maven
+
+```command
+mvn test -Dtest=PageUtilsTest
+```
--- src/test/java/com/rortegat/genericpageable/util/PageUtilsTest.java
+++ src/test/java/com/rortegat/genericpageable/util/PageUtilsTest.java
@@ -1,9 +1,11 @@
 package com.rortegat.genericpageable.util;
 
+import com.google.gson.Gson;
 import com.rortegat.genericpageable.object.NestedNestedObject;
 import com.rortegat.genericpageable.object.NestedObject;
 import com.rortegat.genericpageable.object.NoGettersObject;
 import com.rortegat.genericpageable.object.WrapperObject;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -13,7 +15,10 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Slf4j
 class PageUtilsTest {
+
+    private Gson gson = new Gson();
 
     @Test
     void returnsSortedPage_whenSortListOfObjectsByNestedObjectProperties() {
@@ -28,19 +33,23 @@ class PageUtilsTest {
                 WrapperObject.builder().guid("A").nestedObject(NestedObject.builder().name("Y").nestedNestedObject(NestedNestedObject.builder().id(BigDecimal.valueOf(2.0)).build()).build()).build(),
                 WrapperObject.builder().guid("A").nestedObject(NestedObject.builder().name("X").nestedNestedObject(NestedNestedObject.builder().id(BigDecimal.valueOf(1.0)).build()).build()).build()
         );
-
+        log.info("List of object with other objects as attributes, e.g. {}", list.get(0));
+        log.info("Sorting by guid (ASC), nestedObject.name (ASC) and nestedObject.nestedNestedObject.id (DESC)");
         var sort = Sort.by("guid").and(Sort.by("nestedObject.name").and(Sort.by("nestedObject.nestedNestedObject.id")));
+        log.info("Creating a Pageable object requesting for the first page with the first 10 elements applying the defined sort order");
         var pageable = PageRequest.of(0, 10, sort);
+
         var pageUtils = new PageUtils<WrapperObject>();
         //when
         var orderedPage = pageUtils.listToPage(list, pageable);
+        log.info("Page retrieved: {}", gson.toJson(orderedPage));
         //then
         assertNull(orderedPage.getContent().get(0).getNestedObject().getNestedNestedObject().getId());
         assertEquals(BigDecimal.valueOf(2.0), orderedPage.getContent().get(orderedPage.getContent().size() - 1).getNestedObject().getNestedNestedObject().getId());
     }
 
     @Test
-    void throwsInvocationTargetException_whenGetPropertyDoesNotExistsInObject() {
+    void throwsInvocationTargetException_whenGetObjectPropertyThrowsInvocationTargetException() {
         //given
         var sort = Sort.by("firstName");
         var pageable = PageRequest.of(0, 5, sort);
@@ -53,6 +62,7 @@ class PageUtilsTest {
                 //when
                 pageUtil.listToPage(objectsList, pageable));
 
+        log.error("Thrown exception due properties accessibility: {}", exception.getMessage());
         assertEquals("Object com.rortegat.genericpageable.object.NoGettersObject does not have public access for property firstName or does not exist.", exception.getMessage());
     }
 
